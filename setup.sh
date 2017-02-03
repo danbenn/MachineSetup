65a43f9c0bad57e39ac831d5f1662ccf8153f002
Eric Uriostigue <eric.uriostigue@gmail.com>
2017-02-02 23:35:29 -0500

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# Install vundle before loading new vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+mv .vimrc ..
+mv .bashrc ..
+cd ..
+rm -rf setup
