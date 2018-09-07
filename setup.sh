5c9aae3bc2a234672974a70d99d10281eebad4d9
Dan Bennett <dan.bennett@clinc.com>
2018-09-07 11:02:35 -0400

    add safety checks to setup file

--- setup.sh
+++ setup.sh
@@ -2,9 +2,17 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-mv .vimrc ~/
-mv .bashrc ~/
-mv .tmux.conf ~/
-source ~/.bashrc
-cd ..
-rm -rf setup
+echo "WARNING: this will permanently overwrite your current Vim/Tmux/Bash/Zsh settings"
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    # FORCE Link files in this repo with the configuration files
+    ln -f .vimrc ~/.vimrc
+    ln -f .bashrc ~/.bashrc
+    ln -f .zshrc ~/.zshrc
+    ln -f .tmux.conf ~/.tmux.conf
+
+    # Update the current shell with these preferences
+    source ~/.bashrc
+fi
